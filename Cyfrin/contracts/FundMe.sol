c846bf81371c6bf1e3f669e61a0fe3478c5398b8
rifat87 <tmrifat87@gmail.com>
2024-08-11 15:37:10 +0600

    update2

--- Cyfrin/contracts/FundMe.sol
+++ Cyfrin/contracts/FundMe.sol
@@ -2,22 +2,45 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe{
+    using PriceConverter for uint256;
+
+    uint256 public minimumUsd = 5e18;
+
+    address[] public funders;
+
+    mapping(address funders => uint256 amountFunded) public addressToAmountFunded;
+
     function fund() public payable {
         //Allow users to send $
         //Have a minimum $ sent
         // 1. How do we send ETH to this contract?
-        require(msg.value > 1e18, "didn't sent enough ETHH");
-    }
-    //function withdraw() public{}
+        require(msg.value.getConversionRate() >= minimumUsd, "didn't sent enough ETHH");
+        funders.push(msg.sender);
 
-    function getPrice() public {
-        //Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        //ABI
+        addressToAmountFunded[msg.sender] += msg.value;
     }
-    function getConversionRate() public {
 
+    //withdraw the fund
+    function withdraw() public{
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) 
+        {
+            address funder =funders[funderIndex];
+            addressToAmountFunded[funder] = 0;    
+        }
+
+            // reset the array
+        funders = new address[](0);
+
+        //transer
+        // send
+        // call
+
+
+        //msg.sender = address
+        //payable(msg.sender) = payable address
+        payable(msg.sender).transfer(address(this).balance);
     }
 }
