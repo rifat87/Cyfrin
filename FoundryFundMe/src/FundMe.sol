21dea2af348a679bf073061ffae35eed902ddb3b
Rifat <tmrifat87@gmail.com>
2024-08-21 22:15:25 +0600

    update8

--- FoundryFundMe/src/FundMe.sol
+++ FoundryFundMe/src/FundMe.sol
@@ -1,82 +1,87 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+// Note: The AggregatorV3Interface might be at a different location than what was in the video!
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
-// we can use custom error instead of require for more gas optimization and this is the most updated one
-error FundMe_NotOwner();
+error NotOwner();
 
-contract FundMe{
+contract FundMe {
     using PriceConverter for uint256;
 
-    //constant and immutable gives us the ability to optimize the gas cost and constant variable use All capital letter for the variable
-    // view function has the gas cost when it is called.
-    // for immutable variable we use i_ for better understanding
-    uint256 public constant MINIMUM_USD = 5e18;
-
+    mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
 
-    mapping(address funders => uint256 amountFunded) public addressToAmountFunded;
+    // Could we make this constant?  /* hint: no! We should make it immutable! */
+    address public /* immutable */ i_owner;
+    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
 
-    address public immutable i_owner;
-
-    constructor(){
+    constructor() {
         i_owner = msg.sender;
     }
 
     function fund() public payable {
-        //Allow users to send $
-        //Have a minimum $ sent
-        // 1. How do we send ETH to this contract?
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "didn't sent enough ETHH");
-        funders.push(msg.sender);
-
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
     }
 
-    //withdraw the fund
-    function withdraw() public onlyOwner{
-
-        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) 
-        {
-            address funder =funders[funderIndex];
-            addressToAmountFunded[funder] = 0;    
-        }
-
-            // reset the array
-        funders = new address[](0);
-
-        //transer
-        // send
-        // call
-
-
-        //msg.sender = address
-        //payable(msg.sender) = payable address
-        // payable(msg.sender).transfer(address(this).balance);
-
-        // //send
-        // bool sendSuccess = payable (msg.sender).send(address(this).balance);
-        // require(sendSuccess, "Send failed");
-
-        //call is the main one
-        (bool callSuccess, )= payable(msg.sender).call{value: address(this).balance}("");
-        require(callSuccess, "Call failed");
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
     }
 
     modifier onlyOwner() {
-        // for  withdraw we have to varify it is the owner
-        // require(msg.sender == i_owner, "Must be owner!");
-        if(msg.sender != i_owner) { revert FundMe_NotOwner();} // This is more gas efficient
+        // require(msg.sender == owner);
+        if (msg.sender != i_owner) revert NotOwner();
         _;
     }
 
-    receive() external payable { 
-        fund();
+    function withdraw() public onlyOwner {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+        // call
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
     }
-    fallback() external payable { 
+    // Explainer from: https://solidity-by-example.org/fallback/
+    // Ether is sent to contract
+    //      is msg.data empty?
+    //          /   \
+    //         yes  no
+    //         /     \
+    //    receive()?  fallback()
+    //     /   \
+    //   yes   no
+    //  /        \
+    //receive()  fallback()
+
+    fallback() external payable {
         fund();
     }
 
+    receive() external payable {
+        fund();
+    }
 }
+
+// Concepts we didn't cover yet (will cover in later sections)
+// 1. Enum
+// 2. Events
+// 3. Try / Catch
+// 4. Function Selector
+// 5. abi.encode / decode
+// 6. Hash with keccak256
+// 7. Yul / Assembly
--- FoundryFundMe/test/FundMeTest.t.sol
+++ FoundryFundMe/test/FundMeTest.t.sol
@@ -32,4 +32,19 @@ contract FundMeTest is Test{
 
         assertEq(fundMe.i_owner(), address(this)); // Here the this will check with the current address.
     }
+
+    //What can we do to work with addresses outside out system?
+    // 1. Unit
+    //      - Testing a specific part of our code
+    // 2. Integration
+    //      - Testing our code works with other parts of our code
+    // 3. Forced
+    //      - Testing our code on a simulated real environment
+    // 4. staging 
+    //      - Testing our code in a real environment that is not prod
+    function testPricedFeedVersionIsAccurate() public {
+        uint256 version = fundMe.getVersion();
+        console.log("The version is: ", version);
+        assertEq(version, 4);
+    }
 }
