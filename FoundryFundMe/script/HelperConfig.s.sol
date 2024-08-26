d1cdf16e7fd69c83975bb16d73d895e5e9089657
Rifat <tmrifat87@gmail.com>
2024-08-27 00:51:41 +0600

    update12

--- FoundryFundMe/script/HelperConfig.s.sol
+++ FoundryFundMe/script/HelperConfig.s.sol
@@ -8,11 +8,15 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script{
     //If we are on a local anvil, we deploy mocks
     // otherwise, grab the existing address from the live networkkkk
     NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
 
     struct NetworkConfig {
         address priceFeed; // ETH/USD price feed address
@@ -26,7 +30,7 @@ contract HelperConfig is Script{
             activeNetworkConfig = getMainnetEthConfig();
         }
         else {
-            activeNetworkConfig = getAnvilEthConfig();
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
         }
     }
     function getSepoliaEthConfig() public pure returns(NetworkConfig memory) {
@@ -46,14 +50,25 @@ contract HelperConfig is Script{
         return ethConfig;
     }
 
-    function getAnvilEthConfig() public returns(NetworkConfig memory) {
+    function getOrCreateAnvilEthConfig() public returns(NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;            
+        }
+        
         // price feed address
 
         // 1. Deploy the mocks
         // 2. Return the mock adress
 
         vm.startBroadcast();
-        
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS, INITIAL_PRICE);
         vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
+
+        return anvilConfig;
     }
 }
--- FoundryFundMe/test/FundMeTest.t.sol
+++ FoundryFundMe/test/FundMeTest.t.sol
@@ -14,10 +14,18 @@ contract FundMeTest is Test{
 
     /* Context: In Foundry, assertEq is commonly used in tests to assert that two values are equal. However, this function is part of the DSTest library, which provides various assertion functions for testing purposes.*/
     FundMe fundMe;
+
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;//100000000000000000
+    uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
+
     function setUp() external {
         // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf309aDC325306);
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+
+        vm.deal(USER, STARTING_BALANCE);
     }
 
     function testMinimumDollarIsFive() public {
@@ -30,11 +38,11 @@ contract FundMeTest is Test{
     }
 
     function testOwnerIsMsgSender() public {
-        console.log(fundMe.i_owner());
+        console.log(fundMe.getOwner());
         console.log(". this is msg sender", msg.sender);
         // assertEq(fundMe.i_owner(), msg.sender); this will not same because, the fundMe.i_owner() is actually our local device and the msg.sender is the acutal owner who actually transacts.
 
-        assertEq(fundMe.i_owner(), msg.sender); // Here the this will check with the current address.
+        assertEq(fundMe.getOwner(), msg.sender); // Here the this will check with the current address.
     }
 
     //What can we do to work with addresses outside out system?
@@ -54,4 +62,93 @@ contract FundMeTest is Test{
 
     //Modular deployments
     //Modular testing
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert(); // Hey, the next line, should revert;
+        // assert(This tx fails/reverts)
+
+        fundMe.fund(); // send 0 value
+    } 
+
+    function testFundUpdatesFunddedDataStructure() public{
+        vm.prank(USER);// The next TX will be sent by USER
+        
+        fundMe.fund{value: SEND_VALUE}();
+
+        uint256 amontFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amontFunded, SEND_VALUE);
+    }
+
+    function testAddsFunderToArrayOfFunders() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE} ();
+
+        address funder = fundMe.getFunder(0);
+
+        assertEq(funder, USER);
+    }
+
+    modifier funded() {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        _;
+    }
+    function testOnlyOwnerCanWithdraw() public funded{
+        vm.prank(USER);
+        vm.expectRevert();
+        fundMe.withdraw();
+    }
+
+    function testWithDrawWithASingleFunder() public funded {
+        // Arrange
+        uint256 startingOwnerBalace = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        // Act
+        uint256 gasStart = gasleft(); // 100
+        vm.txGasPrice(GAS_PRICE);
+        vm.prank(fundMe.getOwner()); // c:200
+        fundMe.withdraw(); // should have spent gass
+
+        uint256 gasEnd = gasleft();
+        uint256 gasUsed = (gasStart-gasEnd) * tx.gasprice;
+        console.log(gasUsed);
+
+        //Assert
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        assertEq(endingFundMeBalance, 0);
+        assertEq(startingFundMeBalance + startingOwnerBalace, endingOwnerBalance);
+    }
+
+    function testWithdrawFromMultipleFunders() public funded {
+        // Arrange
+        
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1;
+
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            // vm.prank new address
+            // vm.deal new address
+            //address()
+            hoax(address(i), SEND_VALUE);  // don't know what is hoax
+
+            fundMe.fund{value: SEND_VALUE}();
+            // fud the fundMe
+
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+
+        // Act
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        // Assert
+        assert(address(fundMe).balance == 0);
+        assert(startingFundMeBalance + startingOwnerBalance == fundMe.getOwner().balance);
+    }
 }
