4dc01c5e4a7b9d0e43e0b850866245ea7af5605f
Rifat <tmrifat87@gmail.com>
2024-09-10 01:51:32 +0600

    neadsfsda

---
+++ foundry-smart-contract-lottery/src/Raffle.sol
@@ -0,0 +1,101 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts@1.2.0/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+
+/**
+ * @title A sample Raffle contract
+ * @author Tahzbi Mahmud Rifat
+ * @notice This contract is for creating a sample raffle
+ * @dev Implements Chainlink VRFv2.5
+ */
+
+contract Raffle {
+    /* Errors */
+    error Raffle__SendMoreToEnterRaffle(); // it is good to give the error name and use two __ befoe the error.
+    uint256 private immutable i_entranceFee;
+    // @dev The duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    address payable[] private s_players;
+    //storage variable 
+    uint256 private s_lastTimeStamp;
+
+    //Events
+    event RaffleEntered(address indexed player);
+
+    constructor(uint256 entranceFee, uint256 interval) {
+        i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+    }
+
+    function enterRaffle() external payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent!");
+        // require(msg.value >= i_entranceFee, SendMoreToEnterRaffle());
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+        s_players.push(payable(msg.sender));
+        // 1. Makes migration easier
+        // 2. Makes front end "indexing" easier
+        emit RaffleEntered(msg.sender);
+    }
+
+    //1. Get a random number 
+    // 2. Use random number to pick a play 
+    // 3. Be automatically called
+    function pickWinner() external {
+        // check to see if enough time has passed using block.timestamp
+
+        // 1000 - 900 = 100, 50
+        if (block.timestamp - s_lastTimeStamp > i_interval) {
+            revert();
+        }
+        // Get our random number 2.5
+        // 1. Request RNG
+        // 2. Get RNG
+
+        // Will revert if subscription is not set and funded.
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: s_keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
+    }
+
+    /**
+     * Getter Functions
+     */
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
