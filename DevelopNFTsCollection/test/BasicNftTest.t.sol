d93e70882497fc1dea9d225dd41315690921cb1c
Rifat <tmrifat87@gmail.com>
2024-10-21 23:07:35 +0600

    update4

--- DevelopNFTsCollection/test/BasicNftTest.t.sol
+++ DevelopNFTsCollection/test/BasicNftTest.t.sol
@@ -8,6 +8,9 @@ import {BasicNft} from "../src/BasicNft.sol";
 contract BasicNftTest is Test {
     DeployBasicNft public deployer;
     BasicNft public basicNft;
+    address public USER = makeAddr("use");
+    string public constant PUG = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
     function setUp() public {
         deployer = new DeployBasicNft();
         basicNft = deployer.run();
@@ -21,6 +24,16 @@ contract BasicNftTest is Test {
         // uint256, bool, address bytes32 can be compared but we can not compare array with array like the string.
         // so the assert(expectedName == actualName); will give use an error
         // so for comparig the array in solidty we can use Hash for those string which will then compared.
-        assert(expectedName == actualName);
+        // assert(expectedName == actualName);
+
+        assert(keccak256(abi.encodePacked(expectedName)) == keccak256(abi.encodePacked(actualName)));
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(PUG);
+
+        assert(basicNft.balanceOf(USER) == 1);// assert means dabi kora
+        assert(keccak256(abi.encodePacked(PUG)) == keccak256(abi.encodePacked(basicNft.tokenURI(0))));
     }
 }
